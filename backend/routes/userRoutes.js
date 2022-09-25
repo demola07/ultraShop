ab4c7d2347bad5d6d53667e923f4b40a6b780594
Ademola Sobaki <demolasobaki@gmail.com>
2022-09-25 22:28:01 +0100

    create user authentication endpoint

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router()
+
+import { authUser } from '../controllers/userController.js'
+// import { protect, admin } from '../middleware/authMiddleware.js'
+
+router.post('/login', authUser)
+
+export default router
