f553cc5935502b7e1f68774668ec00d73e2fee46
Ademola Sobaki <demolasobaki@gmail.com>
2022-10-01 20:49:26 +0100

    create screen to list all users, as well as the corresponding reducer and action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-
 import Product from '../components/Product'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
